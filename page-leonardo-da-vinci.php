598cc8b1e4b105456d636f258642528f88af5aa6
Adrian Bueno <adrian.bueno@f1006-06-34133.bmcc.cuny.edu>
2015-12-03 20:48:58 -0500

    midterm updates

--- page-leonardo-da-vinci.php
+++ page-leonardo-da-vinci.php
@@ -19,18 +19,25 @@ get_header(); ?>
 
 			<?php while ( have_posts() ) : the_post(); ?>
             
-            This is my Picasso Page
-
-				<?php get_template_part( 'template-parts/content', 'page' ); ?>
-
-				<?php
-					// If comments are open or we have at least one comment, load up the comment template.
-					if ( comments_open() || get_comments_number() ) :
-						comments_template();
-					endif;
-				?>
-
+                <?php the_title(); ?>
+                <?php the_content();?>
 			<?php endwhile; // End of the loop. ?>
+            
+            <?php rewind_post(); ?>
+            
+            <?php 
+
+                $arg = array('post_type' => 'my_art', 'posts_per_page' => 10 );
+                $loop = new WP_Query ( $arg );
+                while ( $loop -> have_posts() ) : $loop->the_posts();
+
+                echo '<div class="myLeoArt">';
+                the_content();
+                echo '</div>';
+            
+                endwhile; ?>
+            
+            
               <?php
             myRepeat(11);
 
